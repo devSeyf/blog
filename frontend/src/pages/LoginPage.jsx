ae4b5de9f045d73593181b76bbf5018e068d2331
devSeyf <ssaaiiffjjaammaall@gmail.com>
2025-12-22 13:53:51 +0300

    added simple form

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,40 @@
 
-
+import React, { useState } from "react";
 
 export default function LoginPage() {
+      const [email, setEmail] = useState("");      
+  const [password, setPassword] = useState("");  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();  
+  };
     return (
-        <h1>
-        hi Login Page
-        </h1>
+ <form onSubmit={handleSubmit}  className ="max-w-sm mx-auto space-y-4">
+      <div>
+        <label>
+          Email:
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}  
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Password:
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}  
+          />
+        </label>
+      </div>
+
+      <button type="submit">Login</button>  
+    </form>
     )
 }
